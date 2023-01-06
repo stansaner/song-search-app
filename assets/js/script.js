8e3ff930df708cff0d2478d935630d556c44267a
TamzinC <115940593+TamzinC@users.noreply.github.com>
2023-01-06 20:01:08 +0000

    progress code so far to append QR code to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,22 +8,10 @@ urlencoded.append("client_secret", "1ec45ae9502c46db84bf1df9f682dd3e");
 
 
 
-
-// fetch('https://accounts.spotify.com/api/token', {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-//       },
-//     body: urlencoded
-// }).then((response) => {
-//     return response.json()
-// }).then((data) => {
-//     console.log(data);
-// })
-
-
+// This function will bring up a QR code which will link to a google search 
+// of the artist as per user input
 function getQRCode(){
-  var data = "https://api-ninjas.com";
+  var data = "https://api-ninjas.com"; //change to input from user
   var fmt = "png";
   $.ajax({
     method: "GET",
@@ -35,6 +23,15 @@ function getQRCode(){
     contentType: "application/json",
     success: function (result) {
       console.log('QR result', result);
+
+      var mainBody = $('body');
+
+      mainBody.append(`
+            <div>
+                <h3>QR CODE</h3>
+                <img src=${result}>
+            </div> 
+            `);
     },
     error: function ajaxError(jqXHR) {
       console.error("Error: ", jqXHR.responseText);
@@ -42,7 +39,7 @@ function getQRCode(){
   });
 }
 
-
+// This is to get the access token needed to grab the data for the two api's
 function getAccessToken() {
     return fetch("https://accounts.spotify.com/api/token", {
       method: "POST",
@@ -59,7 +56,8 @@ function getAccessToken() {
       });
 }
 
-  
+
+// using the artist id to get the top tracks
 function getTracks(id, token) {
     var artistID = id;
     fetch(`https://api.spotify.com/v1/artists/${artistID}/top-tracks?market=GB`, {
@@ -81,7 +79,10 @@ function getTracks(id, token) {
     })
 }
   
-
+// 2 step process
+// Step 1: obtain artist id
+// Step 2: use artist id to get top tracks for the searched artist
+// this will allow us to grab the artist id
 function getArtists() {
     var artist = "drake";
     getAccessToken().then((token) => {
@@ -118,29 +119,3 @@ getQRCode();
 
 
 
-
-// Get info:
-// data.tracks[0].album.images[1] OR data.tracks.0.album.images[1]
-// 
-
-
-
-/////qr-code  //
-//https://api-ninjas.com/api/qrcode////
-// var data = "https://api-ninjas.com";
-// var fmt = "jpg";
-// $.ajax({
-//   method: "GET",
-//   url: "https://api.api-ninjas.com/v1/qrcode?data=" + data + "&format=" + fmt,
-//   headers: {
-//     "X-Api-Key": "ZdYI+Lj/vMMSzi+ktewh/w==89dSZH02W3eHyFfr",
-//     Accept: "image/png",
-//   },
-//   contentType: "application/json",
-//   success: function (result) {
-//     console.log(result);
-//   },
-//   error: function ajaxError(jqXHR) {
-//     console.error("Error: ", jqXHR.responseText);
-//   },
-// });
